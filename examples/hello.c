2d1ceca0e87ca44a4f962954edc1083f074b2adf
Alexander Shabarshin <ashabarshin@gmail.com>
2015-04-10 15:31:17 -0400

    Comments in hello.c about default modes

--- examples/hello.c
+++ examples/hello.c
@@ -1,4 +1,9 @@
-/* hello.c - A.A.Shabarshin  03-APR-2015 */
+/* hello.c - A.A.Shabarshin (April 2015)
+
+Default mode for PIC32NTSC (internal 8MHz) is XOMODE_256x200_MONO with text 32x25
+Default mode for PIC32NTSCQ (external 14.318MHz) is XOMODE_640x200_MONO with text 80x25
+
+*/
 
 #include "xorlib.h"
 
