054f4a5d98e0c34ff56cc26a81bd8418f224ee4a
Alexander Shabarshin <ashabarshin@gmail.com>
2015-04-10 04:14:20 -0400

    combining platform dependent code into xorlib.c

--- xorlib.c
+++ xorlib.c
@@ -29,16 +29,23 @@ THE SOFTWARE.
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
-
-extern unsigned char xorlib_maxcol;
-extern unsigned char xorlib_maxrow;
-extern unsigned char xorlib_curcol;
-extern unsigned char xorlib_currow;
-extern unsigned char xorlib_curmode;
+#ifdef TERM
+#include <ncurses.h>
+#endif
 
 #define XORLIB_TEMPSTR_SZ 1024
 
 char xorlib_tempstr[XORLIB_TEMPSTR_SZ];
+
+unsigned long xorlib_frames = 0;
+unsigned long xorlib_seconds = 0;
+short xorlib_curline = 0;
+unsigned char xorlib_maxcol = 79;
+unsigned char xorlib_maxrow = 24;
+unsigned char xorlib_curcol = 0;
+unsigned char xorlib_currow = 0;
+unsigned char xorlib_curmode = -1;
+unsigned char xorlib_first = 1;
 
 #ifdef DEBUG
 FILE* xorlib_debug = NULL;
@@ -88,3 +95,77 @@ int xoprintf(char *s,...)
  }
  return o;
 }
+
+void xoexit(void)
+{
+#ifdef DEBUG
+  if(xorlib_debug!=NULL) fprintf(xorlib_debug,"xoexit\n");
+#endif
+  nodelay(stdscr, FALSE);
+  getch();
+#ifdef DEBUG
+  if(xorlib_debug!=NULL) fclose(xorlib_debug);
+#endif
+  endwin();
+}
+
+void xofirst(void)
+{
+  xorlib_first = 0;
+  atexit(xoexit);
+  initscr();
+  raw();
+  keypad(stdscr, TRUE);
+  noecho();
+  nodelay(stdscr, TRUE);
+#ifdef DEBUG
+  xorlib_debug = fopen("xorlib.out","wt");
+  if(xorlib_debug!=NULL) fprintf(xorlib_debug,"xofist\n");
+#endif
+}
+
+unsigned long xoconfig(void)
+{
+  return 0;
+}
+
+unsigned long xocontrols(void)
+{
+  unsigned long controls = 0;
+  switch(getch())
+  {
+     case KEY_LEFT:  controls |= XOCONTROL_LEFT;  break;
+     case KEY_RIGHT: controls |= XOCONTROL_RIGHT; break;
+     case KEY_UP:    controls |= XOCONTROL_UP;    break;
+     case KEY_DOWN:  controls |= XOCONTROL_DOWN;  break;
+     case ' ':       controls |= XOCONTROL_FIRE;  break;
+  }
+  return controls;
+}
+
+void xowaitvblank(void)
+{
+}
+
+int xochar(unsigned char x, unsigned char y, char c)
+{
+  int i,j;
+  if(xorlib_first) xofirst();
+#ifdef DEBUG
+  if(xorlib_debug!=NULL) fprintf(xorlib_debug,"xochar %i,%i,'%c'#%2.2X\n",x,y,(c>=32)?c:' ',c);
+#endif
+  if(x>=0 && x<=xorlib_maxcol && y>=0 && y<=xorlib_maxrow)
+  {
+     mvaddch(y,x,c);
+  }
+  else
+  {
+#ifdef DEBUG
+     if(xorlib_debug!=NULL) fprintf(xorlib_debug,"ERROR: Out of bounds!\n");
+#endif
+  }
+  refresh();
+  return 1;
+}
+
+
